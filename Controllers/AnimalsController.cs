79d9faf7e949e4a2b960ef6f3bd6593a5b918210
Young Liu <youngzliu3@gmail.com>
2019-10-14 21:00:34 -0700

    add get and post methods for api

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CretaceousPark.Models;
+
+namespace CretaceousPark.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private CretaceousParkContext _db;
+
+    public AnimalsController(CretaceousParkContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+
+    // GET api/animals/5
+    [HttpGet("{id}")]
+    public ActionResult<Animal> Get(int id)
+    {
+        return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+  }
+}
